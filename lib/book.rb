0dded599a780b6a6c586f42587f2f5628352dc7c
Norah Taylor <emt368@gmail.com>
2022-06-09 12:14:33 -0400

    commit tree 1481ea4179ff9b3d89df37338a8f99826287ba70

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,27 @@
+require 'pry'
 
+class AllBooks 
+    attr_reader :title
+    attr_accessor :author, :page_count, :genre
 
+     def initialize(title) 
+        @title = title
+    end
+end 
+
+class Book < AllBooks
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end 
+
+class NewBook < AllBooks
+    def new_page(num)
+        puts "I am turing to page #{num}!!"
+    end 
+end 
+
+book = Book.new("book title")
+newBook = NewBook.new("I am a new book title")
+
+bindng.pry 
